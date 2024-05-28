5c312c3223adcba4e16f148ba4a5af3d3c3854d0
Michael Guse <guse.michael@gmail.com>
2024-05-28 13:07:36 -0500

    Added debug info to script and elevation cut-off

--- horizon_moon.sh
+++ horizon_moon.sh
@@ -2,8 +2,9 @@
 
 local range="4d"          #default
 local positional=()
+local debug="false"
 local usage=(
-    "horizon_moon.sh [-h|--help] [-r|--range <number of days|default('4d')>]"
+    "horizon_moon.sh [-h|--help] [-d|--debug] [-r|--range <number of days|default('4d')>]"
 )
 
 opterr() { echo >&2 "optparsing_demo: Unknown option '$1'" }
@@ -12,6 +13,7 @@ while (( $# )); do
     case $1 in
         --)                 shift; positional+=("${@[@]}"); break  ;;
         -h|--help)          printf "%s\n" $usage && return         ;;
+        -d|--debug)         debug="true"                           ;;
         -r|--range)         shift; range=$1                        ;;
         -*)                 opterr $1 && return 2                  ;;
         *)                  positional+=("${@[@]}"); break         ;;
@@ -21,8 +23,20 @@ done
 
 startDate=`date "+%Y-%m-%d"`
 endDate=`date -v+$range "+%Y-%m-%d"`
+strQuantities="'4,10'"
 
-#echo $startDate
-#echo $endDate
+if [[ $debug == "true" ]] 
+then
+    echo "Debug Info"
+    echo "--------------------------------"
+    echo "Start Date: ${startDate}"
+    echo "End Date: ${endDate}"
+    echo "Quantities: ${strQuantities}"
+    
+    echo "\n"
+    echo 'Press any key to continue...'; read -k1 -s
 
-curl -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&QUANTITIES\='10' | less
+    curl -v -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\='0'\&QUANTITIES\="$strQuantities" | less
+else
+    curl -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\='0'\&QUANTITIES\="$strQuantities" | less
+fi
