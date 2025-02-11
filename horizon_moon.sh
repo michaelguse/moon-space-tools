7057a72d268dec808251d966bc6dbff204e2b3bc
Michael Guse <guse.michael@gmail.com>
2025-02-11 08:42:16 -0600

    Added parameter to control ELEV_CUT variable

--- horizon_moon.sh
+++ horizon_moon.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-local range="4d"          #default
+local range="4d"            #default
+local visible="0"             #default
 local positional=()
 local debug="false"
 local usage=(
-    "horizon_moon.sh [-h|--help] [-d|--debug] [-r|--range <number of days|default('4d')>]"
+    "horizon_moon.sh [-h|--help] [-d|--debug] [-v|--visible <is moon visible|default('0')>] [-r|--range <number of days|default('4d')>]"
 )
 
 opterr() { echo >&2 "horizon_moon: Unknown option '$1'" }
@@ -14,6 +15,7 @@ while (( $# )); do
         --)                 shift; positional+=("${@[@]}"); break  ;;
         -h|--help)          printf "%s\n" $usage && return         ;;
         -d|--debug)         debug="true"                           ;;
+        -v|--visible)       shift; visible=$1                      ;;
         -r|--range)         shift; range=$1                        ;;
         -*)                 opterr $1 && return 2                  ;;
         *)                  positional+=("${@[@]}"); break         ;;
@@ -24,6 +26,7 @@ done
 startDate=`date "+%Y-%m-%d"`
 endDate=`date -v+$range "+%Y-%m-%d"`
 strQuantities="'4,10'"
+elevCut=$visible
 
 if [[ $debug == "true" ]] 
 then
@@ -32,11 +35,12 @@ then
     echo "Start Date: ${startDate}"
     echo "End Date: ${endDate}"
     echo "Quantities: ${strQuantities}"
+    echo "Visible: ${elevCut}"
     
     echo "\n"
     echo 'Press any key to continue...'; read -k1 -s
 
-    curl -v -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\='0'\&QUANTITIES\="$strQuantities" | cat 
+    curl -v -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\=$elevCut\&QUANTITIES\="$strQuantities"
 else
-    curl -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\='0'\&QUANTITIES\="$strQuantities" | cat
+    curl -s https://ssd.jpl.nasa.gov/api/horizons.api\?format\=text\&COMMAND\='301'\&OBJ_DATA\='NO'\&MAKE_EPHEM\='YES'\&EPHEM_TYPE\='OBSERVER'\&CENTER\='759'\&START_TIME\=$startDate\&STOP_TIME\=$endDate\&TIME_ZONE\='-6'\&STEP_SIZE\='1h'\&ELEV_CUT\=$elevCut\&QUANTITIES\="$strQuantities"
 fi
